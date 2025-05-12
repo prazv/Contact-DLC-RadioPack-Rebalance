16fd17d8b05b1253af6d84529830996e74b9bc40
prazv <64920464+prazv@users.noreply.github.com>
2025-05-12 14:05:44 +0300

    Add files via upload

---
+++ Addons/config.cpp
@@ -0,0 +1,49 @@
+class CfgPatches {
+    class BackpackOverride {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {};
+    };
+};
+
+class CfgVehicles {
+    class Bag_Base;
+
+    class B_RadioBag_01_black_F:Bag_Base  {
+        mass = 30; 
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_digi_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_eaf_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_ghex_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_hex_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_mtp_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_tropic_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    };
+    class B_RadioBag_01_oucamo_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;  
+    };
+    class B_RadioBag_01_wdl_F:Bag_Base  {
+        mass = 30;
+        maximumLoad = 140;
+    }
+};
---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 0;
+name = "Contact DLC Radiopack Rebalance";
+timestamp = 5250512460328060127;
